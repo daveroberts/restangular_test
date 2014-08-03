05ee15bbb69de098e3e1a0d4ff8bc732642839e2
Dave Roberts <david.a.roberts@gd-ais.com>
2014-08-02 23:14:39 -0400

    Adding javascript libraries

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
 //= require_tree .
+//= require jquery-2.1.1
+//= require lodash
+//= require angular
+//= require restangular
